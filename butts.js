aff50f79d48b5f23bd63c0c145abf89b4370f35b
Aehmlo Lxaitn <aehmlo@aehmlo.com>
2015-03-07 14:06:54 -0600

    Cleaning.

--- butts.js
+++ butts.js
@@ -1,20 +1,18 @@
 function doText(node)  {
 	var v = node.nodeValue;
-	
-    v = v.replace(/\bInternet of Things\b/g, "Butts");
-    v = v.replace(/\bInternet of things\b/g, "Butts");
+	v = v.replace(/\bInternet of Things\b/g, "Butts");
+	v = v.replace(/\bInternet of things\b/g, "Butts");
 	v = v.replace(/\binternet of things\b/g, "butts");
 	v = v.replace(/\binternet of Things\b/g, "butts");
-	
 	node.nodeValue = v;
 }
 
 var walk = function(node) {
 	var child, next;
 	switch(node.nodeType) {
-		case 1:  //Element,
-		case 9:  //Document,
-		case 11: //Document fragment
+		case 1:  // Element,
+		case 9:  // Document,
+		case 11: // Document fragment
 			child = node.firstChild;
 			while(child) {
 				next = child.nextSibling;
